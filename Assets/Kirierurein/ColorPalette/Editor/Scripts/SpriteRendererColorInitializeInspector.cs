941deb7f7dee090ba2956bb7f751621bf56f5e5c
kirierurein <kirierurein@outlook.jp>
2019-08-29 20:30:01 +0900

    SpriteRendererのColorを設定する機能の作成

---
+++ Assets/Kirierurein/ColorPalette/Editor/Scripts/SpriteRendererColorInitializeInspector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace KrColorPalette
+{
+    [CustomEditor(typeof(SpriteRendererColorInitializer))]
+    [CanEditMultipleObjects]
+    public class SpriteRendererColorInitializeInspector : ColorInitializeInspectorBase
+    {
+        private SerializedProperty  targetGraphic       = null;
+
+        protected override void OnEnable()
+        {
+            targetGraphic = serializedObject.FindProperty("target");
+            base.OnEnable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(targetGraphic);
+            base.OnInspectorGUI();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        /// <summary>
+        /// 色の更新
+        /// </summary>
+        protected override void UpdateColor(Color color)
+        {
+            if(targetGraphic.objectReferenceValue != null)
+            {
+                SpriteRenderer graphic = (SpriteRenderer)targetGraphic.objectReferenceValue;
+                graphic.color = color;
+            }
+        }
+    }
+}
---
+++ Assets/Kirierurein/ColorPalette/Scripts/SpriteRendererColorInitializer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace KrColorPalette
+{
+    /// <summary>
+    /// 指定されたColorをSpriteRendererのcolorに設定するクラス
+    /// SpriteRendererColorInitializeInspectorによってInspectorが拡張されています
+    /// </summary>
+    public class SpriteRendererColorInitializer : ColorInitializerBase
+    {
+        [SerializeField]
+        private SpriteRenderer target   = null;
+
+        private void Awake()
+        {
+            if(target != null)
+            {
+                target.color = color;
+            }
+        }
+    }
+}
